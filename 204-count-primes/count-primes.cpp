bd567a4a88022779f6269030841c15142c505988
Pavan C K <pavanck849@gmail.com>
2026-03-05 20:20:25 +0530

    Time: 166 ms (74.65%) | Memory: 12.7 MB (64.88%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        //This is part of the Sieve of Eratosthenes used in Count Primes.
         if(n <= 2) return 0;
 
         vector<bool> isPrime(n, true);
@@ -9,7 +10,7 @@ public:
 
         for(int i = 2; i * i < n; i++) {
             if(isPrime[i]) {
-                for(int j = i * i; j < n; j += i) {
+                for(int j = i * i; j < n; j += i) {//multiple of the number
                     isPrime[j] = false;
                 }
             }
