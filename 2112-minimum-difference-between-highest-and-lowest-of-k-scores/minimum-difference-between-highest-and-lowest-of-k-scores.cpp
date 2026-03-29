b087d65b47080ecd08a8cf774e14b432c8ff70e2
Pavan C K <pavanck849@gmail.com>
2026-03-29 20:25:39 +0530

    Time: 3 ms (42.77%) | Memory: 17.3 MB (21.89%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
         
-        sort(nums.begin(), nums.end());
-        
         int n = nums.size();
+        sort(nums.begin(),nums.end());
         int result = INT_MAX;
-
-        for(int i = 0; i <= n - k; i++) {
-            int diff = nums[i + k - 1] - nums[i];
-            result = min(result, diff);
+        for(int i=0; i<=n-k; i++)
+        {
+            int dif = nums[i+k-1]-nums[i];
+            result = min(result,dif);
         }
-
-        return result;
+       return result;
     }
 };
