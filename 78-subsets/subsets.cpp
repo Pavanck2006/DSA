e4db3742533e34bd40aaa3254913a6f475233dcc
Pavan C K <pavanck849@gmail.com>
2026-05-15 22:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (18.55%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+       
+ void Recursion(int idex,vector<int>& ans,vector<int>& nums, vector<vector<int>>& result,int n)
+        {
+            if(idex==n)
+            {
+                   result.push_back(ans);
+                return;
+
+            }
+            Recursion(idex+1,ans,nums,result,n);
+            ans.push_back(nums[idex]);
+            Recursion(idex+1,ans,nums,result,n);
+            ans.pop_back();
+            
+        }
+    vector<vector<int>> subsets(vector<int>& nums) {
+       
+        vector<int>ans;
+        vector<vector<int>> result;
+         Recursion(0, ans,nums,result,nums.size());
+         return result;
+    }
+};
