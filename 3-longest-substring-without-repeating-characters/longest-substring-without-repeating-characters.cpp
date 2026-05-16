58d5576a19a8ed2c8b94ec6d3b32f27202a7ab89
Pavan C K <pavanck849@gmail.com>
2026-05-16 22:38:45 +0530

    Time: 26 ms (11.23%) | Memory: 14.3 MB (20.85%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -37,24 +37,19 @@ public:
         }
 
         return ans;*/
-        unordered_set<char> st;
-
-int left = 0;
-int count = 0;
-
-for(int right = 0; right < s.size(); right++)
-{
-    while(st.find(s[right]) != st.end())
-    {
-        st.erase(s[left]);
-        left++;
-    }
-
-    st.insert(s[right]);
-
-    count = max(count, right - left + 1);
-}
-
-return count;
+        unordered_set<char>st;
+        int left=0;
+        int count=0;
+        for(int right=0;right<s.size();right++)
+        {
+           while(st.find(s[right]) != st.end())
+            {
+              st.erase(s[left]);
+              left++;
+            }
+            st.insert(s[right]);
+             count= max(count,right-left+1);
+        }
+        return count;
     }
 };
