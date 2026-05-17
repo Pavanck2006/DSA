5201fc5d9dc8fa3d5f5d5c44cdb062e99bfbb153
Pavan C K <pavanck849@gmail.com>
2026-05-17 07:21:50 +0530

    Time: 24 ms (45.02%) | Memory: 36.8 MB (75.47%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
       unordered_set<int> s1(nums1.begin(), nums1.end());
-      vector<int> a;
-vector<int> b;
-vector<vector<int>>ans;
        unordered_set<int> s2(nums2.begin(), nums2.end());
+      vector<vector<int>>ans(2);
+      
       
        for(auto x:s1)
        {
        if(s2.find(x)==s2.end())
         {
-            a.push_back(x);
+            ans[0].push_back(x);
         }
        }
        for(auto y:s2)
        {
         if(s1.find(y)==s1.end())
         {
-        b.push_back(y);
+        ans[1].push_back(y);
         }
 
        }
-       ans.push_back(a);
-       ans.push_back(b);
+      
        return ans;
     }
 };
