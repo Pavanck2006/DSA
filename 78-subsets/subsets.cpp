cca6e63de900d82916b8bbeca0e1f5b2bf4a4ba3
Pavan C K <pavanck849@gmail.com>
2026-05-18 14:50:10 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (36.81%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
        
- void Recursion(int idex,vector<int>& ans,vector<int>& nums, vector<vector<int>>& result,int n)
+ void Recursion(int idex,vector<int>& ans,vector<int>& nums, vector<vector<int>>& result)
         {
-            if(idex==n)
+           /* if(idex==n)
             {
                    result.push_back(ans);
                 return;
@@ -12,14 +12,30 @@ public:
             Recursion(idex+1,ans,nums,result,n);
             ans.push_back(nums[idex]);
             Recursion(idex+1,ans,nums,result,n);
+            ans.pop_back();*/
+             result.push_back(ans);
+
+        for(int i = idex; i < nums.size(); i++)
+        {
+            // choose element
+            // if do not need dupplicate subset if you add the if loop 
+            //if(i>idex && nums[i]==nums[i-1])
+            //continue;
+            ans.push_back(nums[i]);
+
+            // recursive call
+            Recursion(i + 1, ans,nums, result);
+
+            // backtrack
             ans.pop_back();
+        }
             
         }
     vector<vector<int>> subsets(vector<int>& nums) {
        
         vector<int>ans;
         vector<vector<int>> result;
-         Recursion(0, ans,nums,result,nums.size());
+         Recursion(0, ans,nums,result);
          return result;
     }
 };
