327bae4ca3bf06ba1f4bded35022cc30cfc92031
Pavan C K <pavanck849@gmail.com>
2026-05-18 14:41:01 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (81.54%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+void subsequence(int index,vector<int>&ans,vector<vector<int>>&result,vector<int>& candidates,int target)
+{
+     if(target==0)
+     {
+        result.push_back(ans);
+     }
+    for(int i=index;i<candidates.size();i++)
+    {
+         if(candidates[i] > target)
+          break;
+
+       if(i>index && candidates[i]==candidates[i-1])
+       continue;
+      
+        ans.push_back(candidates[i]);
+        subsequence(i+1,ans,result,candidates,target-candidates[i]);
+        ans.pop_back();
+    }
+}
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>result;
+        vector<int>ans;
+         subsequence(0,ans,result,candidates,target);
+    return result;
+    }
+};
