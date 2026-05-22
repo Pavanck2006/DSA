24eb3a023dcc21b51b8b13599faeb53d9a7971c2
Pavan C K <pavanck849@gmail.com>
2026-05-22 17:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (54.63%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        int row = 0;
+        int col = 0;
+        int sum = 0;
+
+        while(col < m && row < n)
+        {
+            if(m % 2 == 0)
+            {
+                while(col < m && row < n)
+                {
+                    sum += mat[row][col];
+                    row++;
+                    col++;
+                }
+
+                row = 0;
+                col = n - 1;
+
+                while(col >= 0 && row < n)
+                {
+                    
+
+                    sum += mat[row][col];
+                    row++;
+                    col--;
+                }
+            }
+            else
+            {
+                while(col < m && row < n)
+                {
+                    sum += mat[row][col];
+                    row++;
+                    col++;
+                }
+
+                row = 0;
+                col = n - 1;
+
+                while(col >= 0 && row < n)
+                {
+                    if(row == col)
+                    {
+                        row++;
+                        col--;
+                        continue;
+                    }
+                    sum += mat[row][col];
+                    row++;
+                    col--;
+                }
+            }
+
+            break;
+        }
+
+        return sum;
+    }
+};
