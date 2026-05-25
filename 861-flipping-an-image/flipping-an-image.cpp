96ff1b15081dee70cc5f1b65f7e4dada2560301e
Pavan C K <pavanck849@gmail.com>
2026-05-25 21:29:14 +0530

    Time: 3 ms (6.72%) | Memory: 12.7 MB (32.70%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n=image.size();
+        vector<int>temp;
+        vector<vector<int>>ans(n,vector<int>(n));
+        for(int i=0;i<n;i++)
+        {
+            int j=n-1;
+            while(j>=0)
+            {
+             temp.push_back(image[i][j]);
+             j--;
+            }
+           ans[i]=temp;// it self act as the single vector
+           // if you access the elemnt with 2d array it work as to arry in this case not possible push condition
+            temp.clear();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(ans[i][j]==1)
+                {
+                    ans[i][j]=0;
+                }
+                else
+                {
+                    ans[i][j]=1;
+                }
+            }
+        }
+        return ans;
+    }
+};
