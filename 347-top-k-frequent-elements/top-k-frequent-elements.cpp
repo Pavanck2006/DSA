b9c7f87da2297db9d513770ac074eb76fd09daa5
Pavan C K <pavanck849@gmail.com>
2026-05-26 22:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (25.50%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -10,9 +10,9 @@ public:
        vector<pair<int,int>> v;
        for(auto x: mp)
        {
-        v.push_back({x.second,x.first});
+        v.push_back({x.second,x.first});//second=freuency,first elment
        }
-        sort(v.rbegin(),v.rend());
+        sort(v.rbegin(),v.rend());// sorted descending order using frequency not number
           for(int i=0; i<k; i++)
             {
             ans.push_back(v[i].second);
