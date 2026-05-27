e6ec9efbecab382d480af5b963d98c0bfe933835
Pavan C K <pavanck849@gmail.com>
2026-05-27 22:06:30 +0530

    Time: 2 ms (6.38%) | Memory: 8.1 MB (50.45%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -3,29 +3,21 @@ class Solution {
 public:
 
     bool detectCapitalUse(string word) {
-        int countu = 0;
-        int countl = 0;
-
-        for(char x : word)
+        int n=word.size()-1;
+        int countu=0;
+        int countl=0;
+        for(char x: word)
         {
-            if(isupper(x))
-                countu++;
-            else
-                countl++;
+          if(isupper(x))
+             countu++;
+             else
+             countl++;
         }
-
-        // ALL uppercase
-        if(countu == word.size())
-            return true;
-
-        // ALL lowercase
-        if(countl == word.size())
-            return true;
-
-        // First letter uppercase, rest lowercase
-        if(isupper(word[0]) && countu == 1)
-            return true;
-
+        if(isupper(word[0]) && countu==1)
+        return true;
+        if(countu==word.size()|| countl==word.size())
+        return true;
         return false;
+        
     }
 };
