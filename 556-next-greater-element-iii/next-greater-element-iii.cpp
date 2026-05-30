435c1662f9cb413a92e731f6a9cc31e70480b0f2
Pavan C K <pavanck849@gmail.com>
2026-05-30 15:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.39%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -4,18 +4,29 @@ public:
         string s=to_string(n);
         int len= s.size();
         int i=len-2;
+/*if (!next_permutation(s.begin(), s.end()))built in function
+            return -1;
 
+        long long ans = stoll(s);
+
+        return (ans > INT_MAX) ? -1 : ans;*/
      while(i>=0 && s[i]>=s[i+1])
+     { // Find pivot
      i--;
+     }
      if(i<0)
+     {
      return -1;
+     }
      int j= len-1;
-     while(s[j]<=s[i])
+     while(s[j]<=s[i]) {
+            // Find just larger element
      j--;
+     }
      swap(s[j],s[i]);
       reverse(s.begin() + i + 1, s.end());
 
-        long long ans = stoll(s);
+        long long ans = stoll(s);//convert agin integer
 
         if (ans > INT_MAX)
             return -1;
