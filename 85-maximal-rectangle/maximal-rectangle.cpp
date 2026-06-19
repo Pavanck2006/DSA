1de5c5177be1c9f08dada62cf8b6a5af27aa6dea
Pavan C K <pavanck849@gmail.com>
2026-06-19 10:08:42 +0530

    Time: 20 ms (14.88%) | Memory: 17.3 MB (97.30%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,58 +1,46 @@
 class Solution {
 public:
-
-    int largestRectangleArea(vector<int>& heights)
-    {
-        stack<int> s;
-        int maxArea = 0;
-        int n = heights.size();
-
-        for(int i=0;i<=n;i++)
-        {
-            int curr = (i==n)?0:heights[i];
-
-            while(!s.empty() && heights[s.top()] >= curr)
-            {
-                int h = heights[s.top()];
-                s.pop();
-
-                int width;
-
-                if(s.empty())
-                    width = i;
-                else
-                    width = i - s.top() - 1;
-
-                maxArea = max(maxArea, h*width);
-            }
-
-            s.push(i);
-        }
-
-        return maxArea;
-    }
-
-
-    int maximalRectangle(vector<vector<char>>& matrix) 
-    {
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        
         int n = matrix.size();
         int m = matrix[0].size();
 
         vector<int> height(m,0);
-
         int ans = 0;
+
 
         for(int i=0;i<n;i++)
         {
+            // Step 1: Convert row into histogram
             for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]=='1')
+                {
                     height[j]++;
+                }
                 else
+                {
                     height[j]=0;
+                }
             }
 
-            ans=max(ans, largestRectangleArea(height));
+
+            // Step 2: Find largest rectangle in histogram
+            for(int j=0;j<m;j++)
+            {
+                int minHeight = height[j];
+
+                for(int k=j;k<m;k++)
+                {
+                    minHeight = min(minHeight,height[k]);
+
+                    int width = k-j+1;
+
+                    int area = minHeight * width;
+
+                    ans = max(ans,area);
+                }
+            }
         }
 
         return ans;
