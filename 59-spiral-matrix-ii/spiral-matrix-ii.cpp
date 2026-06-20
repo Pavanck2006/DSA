20c3f761b13d40a178344533bf3f27f570a612c8
Pavan C K <pavanck849@gmail.com>
2026-06-20 22:12:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (40.79%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>>mat(n,vector<int>(n,0));
+   int srow=0,erow=n-1;
+   int scol=0,ecol=n-1;
+   int count=1;
+   while(srow<=erow && scol<=ecol)
+   {
+    for(int i=scol;i<=ecol;i++)
+    {
+        mat[srow][i]=count++;
+    }
+    for(int j=srow+1;j<=erow;j++)
+    {
+        mat[j][ecol]=count++;
+    }
+    for(int i=ecol-1;i>=scol;i--)
+    {
+        if(srow==erow)
+        break;
+        mat[erow][i]=count++;
+    }
+    for(int j=erow-1;j>srow;j--)
+    {
+        if(scol==ecol)
+        break;
+        mat[j][scol]=count++;
+    }
+    srow++;
+    erow--;
+    scol++;
+    ecol--;
+   }
+   return mat;
+    }
+};
