263d290984b233f71cc7b895c6fdfe3d7e9ac51e
Pavan C K <pavanck849@gmail.com>
2026-07-30 12:18:21 +0530

    Time: 1 ms (94.96%) | Memory: 65.4 MB (65.55%) - LeetSync

---
+++ 790-global-and-local-inversions/global-and-local-inversions.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isIdealPermutation(int[] nums) {
+
+        int n = nums.length;
+        int max = nums[0];
+
+        for (int i = 0; i < n - 2; i++) {
+
+            max = Math.max(max, nums[i]);
+
+            if (max > nums[i + 2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
