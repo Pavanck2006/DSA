6bd8e7eb7426a9318bc2ae30ac61991a061e5fed
Pavan C K <pavanck849@gmail.com>
2026-08-02 22:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.90%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+      int low =0;
+      int high = row*col-1;
+      while(low <= high)
+      {
+        int mid = low+(high-low)/2;
+        int val = matrix[mid/col][mid%col];//this from chatgpt
+        if(val == target)
+        {
+            return true;
+        }
+        if(target < val)
+        {
+            high = mid -1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+      }
+      return false;
+    }
+}
