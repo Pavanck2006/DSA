746ccd367a4cecb9134e5eaabd3f04884e3c6ff5
Pavan C K <pavanck849@gmail.com>
2026-09-23 21:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (30.34%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,74 @@
+
+       class Solution {
+    public:
+    bool uniformArray(vector<int>& nums1) {
+        int sum = 0;
+        int count = 0;
+        vector<int> nums2(nums1.size());
+        if(nums1.size()==1)
+        return true;
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            sum += nums1[i];
+        }
+
+        if(sum % 2 == 0)
+        {
+            bool allEven = true;
+
+            for(int i = 0; i < nums1.size(); i++)
+            {
+                if(nums1[i] % 2 != 0)
+                {
+                    allEven = false;
+                    break;
+                }
+            }
+
+            if(allEven)
+                return true;
+        }
+
+        count = 0;
+
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            if(nums1[i] % 2 != 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == nums1.size())
+        {
+            return true;
+        }
+
+        count = 0;
+
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            for(int j = 0; j < nums1.size(); j++)
+            {
+                if(i != j && nums1[i] % 2 != nums1[j] % 2)
+                {
+                    nums2[i] = nums1[i] - nums1[j];
+
+                    if(nums2[i] % 2 != 0)
+                    {
+                        count++;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        if(count == nums1.size())
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
